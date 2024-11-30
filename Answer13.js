aad4f2fb97c2fbfb7e71237c8adfed6b48a91b74
Kashish <kashishsaraswat69@gmail.com>
2024-11-30 19:53:48 +0530

    Updated code

--- Answer13.js
+++
@@ -1,8 +0,0 @@
-let price = 750;
-let priceCategory = (price >= 1000) ? "Expensive" :
-                    (price >= 500 && price <= 999) ? "Moderate" :
-                    (price > 0 && price < 500) ? "Affordable" :
-                    (price === 0) ? "Free" : "Invalid Price";
-
- 
-console.log(priceCategory);
