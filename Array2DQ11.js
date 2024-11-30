ec5df750ab5281b4a760df9f37821adcda1cab93
Kashish <kashishsaraswat69@gmail.com>
2024-11-30 22:12:29 +0530

    Added

--- Array2DQ11.js
+++ Array2DQ11.js
@@ -1,4 +1,9 @@
 let arr=[[1,2],[3,4],[5,6]];
-console.log(arr[0]);
- console.log(arr[1]);
-  console.log(arr[2]);
+  
+for(let i =0;i<arr.length;i++){
+    let res=" ";
+    for(let j=0;j<arr[0].length;j++){
+        res+=arr[i][j]+" ";
+    }
+    console.log(res);
+}
